2777c469601963b63c0c3e1cfce46d4541e545cf
riyashri07 <riyampct7@gmail.com>
2023-07-30 23:51:08 +0530

    cross update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import Styles from "./Styles/Navbar.module.css";
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+
+
 const Navbar = () => {
     const [mobile, setMobile] = useState(false);
 
@@ -10,19 +13,33 @@ const Navbar = () => {
             <div className={Styles.logo}>
                 <h3>Logo</h3>
             </div>
-            <ul className={Styles.nav_links}>
-                <Link to="/"><li>Home</li></Link>
-                <Link to="/about"><li>About</li></Link>
-                <Link to="/carrers"><li>Carrers</li></Link>
-                <Link to="/contact"><li>Contact</li></Link>
-                <Link to="/login"><li>Login</li></Link>
-                <Link to="/logout"><li>Logout</li></Link>
+            <ul
+                className={mobile ? "nav-links-mobile" : "nav_links"}
+                onClick={() => setMobile(false)}>
+                <Link to="/">
+                    <li>Home</li>
+                </Link>
+                <Link to="/about">
+                    <li>About</li>
+                </Link>
+                <Link to="/carrers">
+                    <li>Carrers</li>
+                </Link>
+                <Link to="/contact">
+                    <li>Contact</li>
+                </Link>
+                <Link to="/login">
+                    <li>Login</li>
+                </Link>
+                <Link to="/logout">
+                    <li>Logout</li>
+                </Link>
             </ul>
 
             <button
                 className={Styles.mobilemenu}
                 onClick={() => setMobile(!mobile)}>
-                <FaBars />
+                {mobile ? <ImCross /> : <FaBars />}
             </button>
         </div>
     );
