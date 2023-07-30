79f502eb149f2e869c7c74b8fbd34ae740d9a5c5
riyashri07 <riyampct7@gmail.com>
2023-07-30 23:24:37 +0530

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react'
-import Styles from "./Styles/Navbar.module.css"
-import { Link } from "react-router-dom"
-import {FaBars} from "react-icons/fa"
+import React, { useState } from "react";
+import Styles from "./Styles/Navbar.module.css";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 const Navbar = () => {
+    const [mobile, setMobile] = useState(false);
 
-    const [mobile, setMobile] = useState(false)
+    return (
+        <div className={Styles.main}>
+            <div className={Styles.logo}>
+                <h3>Logo</h3>
+            </div>
+            <ul className={Styles.nav_links}>
+                <Link to="/"><li>Home</li></Link>
+                <Link to="/about"><li>About</li></Link>
+                <Link to="/carrers"><li>Carrers</li></Link>
+                <Link to="/contact"><li>Contact</li></Link>
+                <Link to="/login"><li>Login</li></Link>
+                <Link to="/logout"><li>Logout</li></Link>
+            </ul>
 
-  return (
-      <div className={Styles.main}>
-          <div className={Styles.left}>
-              <h3>Logo</h3>
-          </div>
-          <div className={Styles.right}>
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
-              <Link to="/carrers">Carrers</Link>
-              <Link to="/contact">Contact</Link>
-              <Link to="/login">Login</Link>
-              <Link to="/logout">Logout</Link>
-          </div>
+            <button
+                className={Styles.mobilemenu}
+                onClick={() => setMobile(!mobile)}>
+                <FaBars />
+            </button>
+        </div>
+    );
+};
 
-          <button className={Styles.mobilemenu} onClick={()=>setMobile(!mobile)}>
-              <FaBars/>
-          </button>
-
-      </div>
-  );
-}
-
-export default Navbar
+export default Navbar;
